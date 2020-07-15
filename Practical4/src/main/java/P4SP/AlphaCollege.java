7fc4ae32f19773a05a2dd6a7ee1a50799ec709bb
Xande <yongzx-wm19@student.tarc.edu.my>
2020-07-15 11:06:47 +0800

    OOPT

--- Practical4/src/main/java/P4SP/AlphaCollege.java
+++ Practical4/src/main/java/P4SP/AlphaCollege.java
@@ -12,7 +12,9 @@ public class AlphaCollege {
         course1.addStudent("Peter Lim");
 
         System.out.println(course1.toString());
-
+        
+        System.out.printf("Total Fees Collected : RM %.2f\n",course1.calFeesCollected());
+        
         System.out.println("Enrolled Student: ");
         for (int x = 0; x < course1.getNoOfStudents(); x++) {
             System.out.println((x + 1) + ". " + course1.getStudentNames(x));
